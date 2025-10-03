04e846afc129146f9057f4e673219a9ea59328d1
Adit Jana <adit.jana.work@gmail.com>
2025-10-03 23:57:32 +0530

    commented jwt flow once enabled from configure then will uncomment

--- src/main/java/com/adit/order_management_service/controller/AuthenticationController.java
+++ src/main/java/com/adit/order_management_service/controller/AuthenticationController.java
@@ -1,30 +1,30 @@
-package com.adit.order_management_service.controller;
-
-import com.adit.order_management_service.security.model.LoginRequest;
-import com.adit.order_management_service.security.util.JwtUtil;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(value = "/v2/auth")
-public class AuthenticationController {
-
-    @PostMapping(value = "/get_auth_token")
-    public ResponseEntity<?> getAuthenticationToken(@RequestBody LoginRequest loginRequest) {
-
-        String username = loginRequest.getUsername();
-        String password = loginRequest.getPassword();
-
-        String role = "admin".equals(username) ? "ROLE_ADMIN" : "ROLE_USER";
-
-        if (("admin".equals(username) && "password".equals(password)) ||
-                ("user".equals(username) && "password".equals(password))) {
-            String token = JwtUtil.generateToken(username, role);
-            return ResponseEntity.ok().body(token);
-        }
-        return ResponseEntity.status(401).body("Invalid credentials");
-    }
-}
+//package com.adit.order_management_service.controller;
+//
+//import com.adit.order_management_service.security.model.LoginRequest;
+//import com.adit.order_management_service.security.util.JwtUtil;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping(value = "/v2/auth")
+//public class AuthenticationController {
+//
+//    @PostMapping(value = "/get_auth_token")
+//    public ResponseEntity<?> getAuthenticationToken(@RequestBody LoginRequest loginRequest) {
+//
+//        String username = loginRequest.getUsername();
+//        String password = loginRequest.getPassword();
+//
+//        String role = "admin".equals(username) ? "ROLE_ADMIN" : "ROLE_USER";
+//
+//        if (("admin".equals(username) && "password".equals(password)) ||
+//                ("user".equals(username) && "password".equals(password))) {
+//            String token = JwtUtil.generateToken(username, role);
+//            return ResponseEntity.ok().body(token);
+//        }
+//        return ResponseEntity.status(401).body("Invalid credentials");
+//    }
+//}
--- src/main/java/com/adit/order_management_service/security/util/JwtUtil.java
+++ src/main/java/com/adit/order_management_service/security/util/JwtUtil.java
@@ -1,61 +1,61 @@
-package com.adit.order_management_service.security.util;
-
-import com.auth0.jwt.JWT;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-
-import javax.crypto.spec.SecretKeySpec;
-import java.security.Key;
-import java.util.Date;
-
-public class JwtUtil {
-
-    private static final String SECRET_KEY = "my-super-duper-secret-key-for-oms";
-
-    private static final long EXPIRATION_TIME = 3600000;
-
-    private static final SignatureAlgorithm SIGNATURE_ALGORITHM = SignatureAlgorithm.HS256;
-
-    private static final Key key = new SecretKeySpec(SECRET_KEY.getBytes(),
-            SIGNATURE_ALGORITHM.getJcaName());
-
-    // generate jst token based on username and role
-    public static String generateToken(String username, String role) {
-        return Jwts.builder()
-                .setSubject(username)
-                .claim("role", role)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SIGNATURE_ALGORITHM, key)
-                .compact();
-    }
-
-    // validate token
-    public static boolean validateToken(String token) {
-        try {
-            Jwts.parser()
-                    .setSigningKey(key)
-                    .parseClaimsJwt(token);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    public static String getUsernameFromToken(String token) {
-        return Jwts.parser()
-                .setSigningKey(key)
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
-    }
-
-    public static String getRoleFromToken(String token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(key)
-                .parseClaimsJws(token)
-                .getBody();
-        return claims.get("role", String.class);
-    }
-}
+//package com.adit.order_management_service.security.util;
+//
+//import com.auth0.jwt.JWT;
+//import io.jsonwebtoken.Claims;
+//import io.jsonwebtoken.Jwts;
+//import io.jsonwebtoken.SignatureAlgorithm;
+//
+//import javax.crypto.spec.SecretKeySpec;
+//import java.security.Key;
+//import java.util.Date;
+//
+//public class JwtUtil {
+//
+//    private static final String SECRET_KEY = "my-super-duper-secret-key-for-oms";
+//
+//    private static final long EXPIRATION_TIME = 3600000;
+//
+//    private static final SignatureAlgorithm SIGNATURE_ALGORITHM = SignatureAlgorithm.HS256;
+//
+//    private static final Key key = new SecretKeySpec(SECRET_KEY.getBytes(),
+//            SIGNATURE_ALGORITHM.getJcaName());
+//
+//    // generate jst token based on username and role
+//    public static String generateToken(String username, String role) {
+//        return Jwts.builder()
+//                .setSubject(username)
+//                .claim("role", role)
+//                .setIssuedAt(new Date())
+//                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+//                .signWith(SIGNATURE_ALGORITHM, key)
+//                .compact();
+//    }
+//
+//    // validate token
+//    public static boolean validateToken(String token) {
+//        try {
+//            Jwts.parser()
+//                    .setSigningKey(key)
+//                    .parseClaimsJwt(token);
+//            return true;
+//        } catch (Exception e) {
+//            return false;
+//        }
+//    }
+//
+//    public static String getUsernameFromToken(String token) {
+//        return Jwts.parser()
+//                .setSigningKey(key)
+//                .parseClaimsJws(token)
+//                .getBody()
+//                .getSubject();
+//    }
+//
+//    public static String getRoleFromToken(String token) {
+//        Claims claims = Jwts.parser()
+//                .setSigningKey(key)
+//                .parseClaimsJws(token)
+//                .getBody();
+//        return claims.get("role", String.class);
+//    }
+//}
