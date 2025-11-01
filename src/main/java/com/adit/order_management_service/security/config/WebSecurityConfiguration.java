e4c28a6b678194b8950f65b6c2656ea24ce0a5e3
Adit Jana <adit.jana.work@gmail.com>
2025-11-02 00:30:26 +0530

    Old security config configuration along with basic username password authentication commented -- before spring security V6

--- src/main/java/com/adit/order_management_service/security/config/WebSecurityConfiguration.java
+++ src/main/java/com/adit/order_management_service/security/config/WebSecurityConfiguration.java
@@ -1,83 +1,112 @@
-package com.adit.order_management_service.security.config;
-
-import com.adit.order_management_service.security.service.CustomAuthenticationEntryPoint;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.RegexRequestMatcher;
-
-@Configuration
+//package com.adit.order_management_service.security.config;
+//
+//import com.adit.order_management_service.security.jwt.JwtAuthFilter;
+//import com.adit.order_management_service.service.UserService;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.AuthenticationProvider;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+//
+//@Configuration
 //@EnableWebSecurity
 //@EnableMethodSecurity
-@AllArgsConstructor
-public class WebSecurityConfiguration {
-
-
-    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-
-    // Define in-memory users with encoded passwords
-    /*@Bean
-    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-
-        UserDetails user = User.builder()
-                .username("user")
-                .password(passwordEncoder.encode("pass"))
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder.encode("admin"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
-    }*/
-
-    // Define security filter chain
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .csrf(csrf-> csrf.disable()) // disable CSRF for APIs
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(new RegexRequestMatcher(".*/public/.*", null)).permitAll() // access all
-                        .requestMatchers("/user/**").hasRole("USER") // user authenticated
-                        .requestMatchers("/admin/**").hasAnyRole("USER","ADMIN") // restricted access
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(basic ->
-                        basic.authenticationEntryPoint(authenticationEntryPoint));  // plug in custom authentication
-        return httpSecurity.build();
-    }
-
-
-
-    /*public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-
-        httpSecurity
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(
-                        auth -> auth.requestMatchers("/public/**").permitAll()
-                                .requestMatchers("/admin/**").hasRole("ADMIN")
-                                .anyRequest().authenticated())
-                .httpBasic(basic-> basic.realmName("my app real name"));
-        return httpSecurity.build();
-
-    }*/
-}
+//public class WebSecurityConfiguration {
+//
+//
+//    //private final CustomAuthenticationEntryPoint authenticationEntryPoint;
+//
+//    private final UserService userDetailsService;
+//    private final JwtAuthFilter jwtAuthFilter;
+//
+//    public WebSecurityConfiguration(UserService userDetailsService, JwtAuthFilter jwtAuthFilter) {
+//        this.userDetailsService = userDetailsService;
+//        this.jwtAuthFilter = jwtAuthFilter;
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    // Define in-memory users with encoded passwords
+//    /*@Bean
+//    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+//
+//        UserDetails user = User.builder()
+//                .username("user")
+//                .password(passwordEncoder.encode("pass"))
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder.encode("admin"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }*/
+//
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+//        return config.getAuthenticationManager();
+//    }
+//
+//    @Bean
+//    public AuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+//        provider.setUserDetailsService(userDetailsService);
+//        provider.setPasswordEncoder(passwordEncoder());
+//        return provider;
+//    }
+//
+//    // Define security filter chain
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity
+//                .csrf(csrf -> csrf.disable()) // disable CSRF for APIs
+//                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/auth/**").permitAll()
+//                        .requestMatchers(HttpMethod.GET, "/health").permitAll()
+//                        .anyRequest().authenticated()
+//
+////                        .requestMatchers(new RegexRequestMatcher(".*/public/.*", null)).permitAll() // access all
+////                        .requestMatchers("/user/**").hasRole("USER") // user authenticated
+////                        .requestMatchers("/admin/**").hasAnyRole("USER", "ADMIN") // restricted access
+////                        .anyRequest().authenticated()
+//                )
+//                .authenticationProvider(authenticationProvider())
+//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+//                /*.httpBasic(basic ->
+//                        basic.authenticationEntryPoint(jwtAuthFilter));*/  // plug in custom authentication
+//        return httpSecurity.build();
+//    }
+//
+//
+//
+//    /*public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//
+//        httpSecurity
+//                .csrf(csrf -> csrf.disable())
+//                .authorizeHttpRequests(
+//                        auth -> auth.requestMatchers("/public/**").permitAll()
+//                                .requestMatchers("/admin/**").hasRole("ADMIN")
+//                                .anyRequest().authenticated())
+//                .httpBasic(basic-> basic.realmName("my app real name"));
+//        return httpSecurity.build();
+//
+//    }*/
+//}
--- src/main/java/com/adit/order_management_service/service/UserService.java
+++ src/main/java/com/adit/order_management_service/service/UserService.java
@@ -1,109 +1,109 @@
-package com.adit.order_management_service.service;
-
-import com.adit.order_management_service.constant.Roles;
-import com.adit.order_management_service.dto.response.UserResponseDto;
-import com.adit.order_management_service.entity.OmsRole;
-import com.adit.order_management_service.entity.UserEntity;
-import com.adit.order_management_service.model.request.UserRequest;
-import com.adit.order_management_service.repo.OmsRoleRepo;
-import com.adit.order_management_service.repo.UserRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-
-import java.util.*;
-import java.util.stream.Collectors;
-
-@Service
-public class UserService implements UserDetailsService {
-
-    @Autowired
-    private UserRepo userRepo;
-
-    @Autowired
-    private OmsRoleRepo omsRoleRepo;
-
-    /*@Autowired
-    private JdbcUserDetailsManager jdbcUserDetailsManager;*/
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    public UserResponseDto createUser(UserRequest userRequest) {
-
-        // check if username already exists
-
-        Set<OmsRole> userRoleList;
-
-        List<Roles> userRoleTagList;
-
-
-        // fetch role from DB
-        userRoleList = userRequest.getRole().stream()
-                .map(roleName -> omsRoleRepo.findByRoleName(roleName.getDesc())
-                        .orElseThrow(() -> new RuntimeException(" Role not found: " + roleName.getDesc())))
-                .collect(Collectors.toSet());
-
-        userRoleTagList = userRequest.getRole().stream().collect(Collectors.toList());
-
-        UserEntity userEntity = userRepo.save(UserEntity.builder()
-                .userName(userRequest.getUserName())
-                .userPassword(passwordEncoder.encode(userRequest.getPassword()))
-                .userRolesTag(userRoleTagList)
-                .userRoles(userRoleList)
-                .build());
-
-        return UserResponseDto.builder()
-                .userId(userEntity.getUserId())
-                .userName(userRequest.getUserName())
-                .rolesList(userRoleList.stream()
-                        .map(r -> r.getRoleName())
-                        .collect(Collectors.toList()))
-                .build();
-    }
-
-    public UserResponseDto getUser(UserRequest userRequest) {
-
-        // This triggers your CustomUserDetailsService.loadUserByUsername
-        // User is authenticated now
-        UserDetails userDetails = loadUserByUsername(userRequest.getUserName());
-
-        UserEntity omsUser = userRepo.findByUserName(userRequest.getUserName())
-                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + userRequest.getUserName()));
-
-        UserResponseDto userResponseDto = null;
-
-        if (Objects.nonNull(userDetails) && Objects.nonNull(userDetails.getUsername())) {
-            userResponseDto = UserResponseDto.builder()
-                    .userId(omsUser.getUserId())
-                    .userName(userDetails.getUsername())
-                    .rolesList(omsUser.getUserRoles().stream()
-                            .map(OmsRole::getRoleDesc)
-                            .collect(Collectors.toList()
-                    ))
-                    .build();
-        }
-        return userResponseDto;
-    }
-
-
-    @Override
-    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-        UserEntity omsUser = userRepo.findByUserName(userName)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + userName));
-
-
-        return User.builder()
-                .username(omsUser.getUserName())
-                .password(omsUser.getUserPassword()) // must already be BCrypt encoded
-                .disabled(Boolean.FALSE.equals(omsUser.getUserEnabled()))
-                .roles(String.valueOf(omsUser.getUserRolesTag().stream()
-                        .map(Roles::getDesc).collect(Collectors.toSet())))    // Spring will add ROLE_ prefix internally
-                .build();
-
-    }
-}
+//package com.adit.order_management_service.service;
+//
+//import com.adit.order_management_service.constant.Roles;
+//import com.adit.order_management_service.dto.response.UserResponseDto;
+//import com.adit.order_management_service.entity.OmsRole;
+//import com.adit.order_management_service.entity.UserEntity;
+//import com.adit.order_management_service.model.request.UserRequest;
+//import com.adit.order_management_service.repo.OmsRoleRepo;
+//import com.adit.order_management_service.repo.UserRepo;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.*;
+//import java.util.stream.Collectors;
+//
+//@Service
+//public class UserService implements UserDetailsService {
+//
+//    @Autowired
+//    private UserRepo userRepo;
+//
+//    @Autowired
+//    private OmsRoleRepo omsRoleRepo;
+//
+//    /*@Autowired
+//    private JdbcUserDetailsManager jdbcUserDetailsManager;*/
+//
+//    /*@Autowired
+//    private PasswordEncoder passwordEncoder;*/
+//
+//    public UserResponseDto createUser(UserRequest userRequest) {
+//
+//        // check if username already exists
+//
+//        Set<OmsRole> userRoleList;
+//
+//        List<Roles> userRoleTagList;
+//
+//
+//        // fetch role from DB
+//        userRoleList = userRequest.getRole().stream()
+//                .map(roleName -> omsRoleRepo.findByRoleName(roleName.getDesc())
+//                        .orElseThrow(() -> new RuntimeException(" Role not found: " + roleName.getDesc())))
+//                .collect(Collectors.toSet());
+//
+//        userRoleTagList = userRequest.getRole().stream().collect(Collectors.toList());
+//
+//        UserEntity userEntity = userRepo.save(UserEntity.builder()
+//                .userName(userRequest.getUserName())
+//                .userPassword(passwordEncoder.encode(userRequest.getPassword()))
+//                .userRolesTag(userRoleTagList)
+//                .userRoles(userRoleList)
+//                .build());
+//
+//        return UserResponseDto.builder()
+//                .userId(userEntity.getUserId())
+//                .userName(userRequest.getUserName())
+//                .rolesList(userRoleList.stream()
+//                        .map(r -> r.getRoleName())
+//                        .collect(Collectors.toList()))
+//                .build();
+//    }
+//
+//    public UserResponseDto getUser(UserRequest userRequest) {
+//
+//        // This triggers your CustomUserDetailsService.loadUserByUsername
+//        // User is authenticated now
+//        UserDetails userDetails = loadUserByUsername(userRequest.getUserName());
+//
+//        UserEntity omsUser = userRepo.findByUserName(userRequest.getUserName())
+//                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + userRequest.getUserName()));
+//
+//        UserResponseDto userResponseDto = null;
+//
+//        if (Objects.nonNull(userDetails) && Objects.nonNull(userDetails.getUsername())) {
+//            userResponseDto = UserResponseDto.builder()
+//                    .userId(omsUser.getUserId())
+//                    .userName(userDetails.getUsername())
+//                    .rolesList(omsUser.getUserRoles().stream()
+//                            .map(OmsRole::getRoleDesc)
+//                            .collect(Collectors.toList()
+//                    ))
+//                    .build();
+//        }
+//        return userResponseDto;
+//    }
+//
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+//        UserEntity omsUser = userRepo.findByUserName(userName)
+//                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + userName));
+//
+//
+//        return User.builder()
+//                .username(omsUser.getUserName())
+//                .password(omsUser.getUserPassword()) // must already be BCrypt encoded
+//                .disabled(Boolean.FALSE.equals(omsUser.getUserEnabled()))
+//                .roles(String.valueOf(omsUser.getUserRolesTag().stream()
+//                        .map(Roles::getDesc).collect(Collectors.toSet())))    // Spring will add ROLE_ prefix internally
+//                .build();
+//
+//    }
+//}
