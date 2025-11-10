67785f76de8cc4b3a61d8af57dc4b13e1927d5e1
Adit Jana <adit.jana.work@gmail.com>
2025-11-10 23:15:12 +0530

    Role based access enabled

--- src/main/java/com/adit/order_management_service/controller/ProductController.java
+++ src/main/java/com/adit/order_management_service/controller/ProductController.java
@@ -5,11 +5,11 @@ import com.adit.order_management_service.model.request.filter.ProductFilterDto;
 import com.adit.order_management_service.model.request.product.ProductDashboardView;
 import com.adit.order_management_service.model.request.product.ProductRequest;
 import com.adit.order_management_service.service.ProductService;
-import org.aspectj.lang.annotation.RequiredTypes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,14 +17,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/v2/product")
 public class ProductController {
-    // add
-    //delete
-    // update
-    // edit
 
     @Autowired
     private ProductService productService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping(value = "/create")
     public ResponseEntity<ProductResponseDto> createProduct(@RequestBody ProductRequest productRequest) {
         ProductResponseDto productResponseDto =
@@ -32,16 +29,17 @@ public class ProductController {
         return new ResponseEntity<>(productResponseDto, HttpStatusCode.valueOf(200));
     }
 
+    @PreAuthorize("hasRole('USER')")
     @GetMapping(value = "/get_details")
     public ResponseEntity<ProductResponseDto> getProductDetails(
-            @RequestParam (value = "productId") Long productId) {
+            @RequestParam(value = "productId") Long productId) {
         ProductResponseDto productResponseDto = productService.getProductDetails(productId);
         return new ResponseEntity<>(productResponseDto, HttpStatusCode.valueOf(200));
     }
 
     @GetMapping(value = "/get_product_list")
     public ResponseEntity<List<ProductResponseDto>> getProductList(
-            @RequestParam (value = "productIds") List<Long> productIds) {
+            @RequestParam(value = "productIds") List<Long> productIds) {
         List<ProductResponseDto> productResponseDtoList = productService.getProductList(productIds);
         return new ResponseEntity<>(productResponseDtoList, HttpStatusCode.valueOf(200));
     }
@@ -54,10 +52,11 @@ public class ProductController {
             @RequestParam(required = false) String sortOrder,
             @RequestBody(required = false) ProductFilterDto productFilterDto) {
         ProductDashboardView productResponsePaginationView =
-               productService.getDashboardView(page, size, sortBy, sortOrder ,productFilterDto);
+                productService.getDashboardView(page, size, sortBy, sortOrder, productFilterDto);
         return ResponseEntity.ok(productResponsePaginationView);
     }
 
+    @PreAuthorize("hasRole('ADMIN')") // admin role added
     @GetMapping("/admin/dashboard")
     public ResponseEntity<ProductDashboardView> getAllTheProductsAdminView(
             @RequestParam(defaultValue = "0", value = "page", required = false) int page,
@@ -65,18 +64,20 @@ public class ProductController {
             @RequestParam(required = false) String sortBy,
             @RequestParam(required = false) String sortOrder,
             @RequestBody(required = false) ProductFilterDto productFilterDto) {
+
         ProductDashboardView productResponsePaginationView =
-                productService.getDashboardView(page, size, sortBy, sortOrder ,productFilterDto);
+                productService.getDashboardView(page, size, sortBy, sortOrder, productFilterDto);
         return ResponseEntity.ok(productResponsePaginationView);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping(value = "/update")
-    public ResponseEntity<ProductResponseDto> updatedProductDetails(@RequestParam (value = "productId") Long productId,
+    public ResponseEntity<ProductResponseDto> updatedProductDetails(@RequestParam(value = "productId") Long productId,
                                                                     @RequestBody ProductRequest productRequest) {
-        ProductResponseDto productResponseDto  =
+        ProductResponseDto productResponseDto =
                 productService.updateProductDetails(productId, productRequest);
         return new ResponseEntity<>(productResponseDto, HttpStatus.OK);
     }
 
-    
+
 }
