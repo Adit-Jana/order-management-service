578d69409679838c7d6206cb6b2f76a9d21fac58
Adit Jana <adit.jana.work@gmail.com>
2025-11-18 22:15:01 +0530

    order entity added

--- src/main/java/com/adit/order_management_service/entity/OrderEntity.java
+++ src/main/java/com/adit/order_management_service/entity/OrderEntity.java
@@ -8,6 +8,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Builder
 @Data
@@ -25,13 +28,19 @@ public class OrderEntity {
     private Long order_id;
     private String order_desc;
     private BigDecimal total_order_amount;
-    private Long product_id;
     private Long user_id;
     private String payment_uid;
-    private Integer product_quantity;
     private Long billing_ref_id;
     private Long shipping_ref_id;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @Version
+    private Long version;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.PERSIST, orphanRemoval = true)
+    private List<OrderItemEntity> items = new ArrayList<>();
 }
